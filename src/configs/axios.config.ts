8922182189a8f257101cf7c06d32dfad4ffce247
daik1i-man <joshqinbekalimboyev@gmail.com>
2024-08-28 23:18:41 +0500

    chages server url

--- src/configs/axios.config.ts
+++ src/configs/axios.config.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosRequestConfig } from "axios";
 
 const axiosOptions: AxiosRequestConfig = {
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://online-pharmacy-server.onrender.com',
     timeout: 10000,
     withCredentials: true,
     headers: {
